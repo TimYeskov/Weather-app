9c1321f29f0e95744f6b9ff4f685e4f98e353b41
Timur <timur.yeskov@gmail.com>
2023-05-27 14:27:01 +0400

    Commit
    Main Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+import AppContext from "../provider/appContext";
+
+function Footer() {
+  const {
+    app: { isDark },
+  } = useContext(AppContext);
+  const anchorStyle = {
+    color: "#C6C6C6",
+  };
+  return (
+    <>
+      <footer className="footer">
+        <a
+          href="https://github.com/TimYeskov"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i class="fa-brands fa-github"></i> <span>GitHub</span>
+        </a>
+    
+     
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
